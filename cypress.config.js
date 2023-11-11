7e99591fa790ba0a8163c76a040d80bdbd9ed16b
samlucax <samuelborgeslucas@gmail.com>
2023-11-11 20:12:33 -0300

    test: adicionando config para report em xml

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     video: true,
+    reporter: 'junit',
+    reporterOptions: {
+      mochaFile: 'cypress/reports/junit/test-results-[hash].xml',
+      toConsole: true
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
