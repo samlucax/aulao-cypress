5ce9fc4d020ba8e85ba2b6cf3a6049064783d0dd
samlucax <samuelborgeslucas@gmail.com>
2023-11-11 21:21:48 -0300

    test: adicionando config de report html

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,19 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     video: true,
-    reporter: 'junit',
+    reporter: 'cypress-multi-reporters',
     reporterOptions: {
-      mochaFile: 'cypress/reports/junit/test-results-[hash].xml',
-      toConsole: true
+      reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+      mochaJunitReporterReporterOptions: {
+        mochaFile: 'cypress/reports/junit/test-results-[hash].xml',
+      },
+      cypressMochawesomeReporterReporterOptions: {
+        charts: true
+      },
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
